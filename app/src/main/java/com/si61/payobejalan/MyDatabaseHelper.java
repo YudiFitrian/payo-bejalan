991aa936ab6a69191f2d5b3a7f30be607610fac7
Yudi Fitriansyah <cleveryf098@gmail.com>
2023-03-09 18:19:18 +0700

    Payo Bejalan Pt3

--- app/src/main/java/com/si61/payobejalan/MyDatabaseHelper.java
+++ app/src/main/java/com/si61/payobejalan/MyDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.si61.payobejalan;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,17 +8,63 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper{
-    public MyDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private Context ctx;
+    private static final String DATABASE_NAME = "db_payo_bejalan";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "tbl_destinasi";
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_NAMA = "nama";
+    private static final String FIELD_ALAMAT = "alamat";
+    private static final String FIELD_JAM = "jam";
+
+    //factori adalah libary external tapi tidak di gunakan dan di ganti null
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.ctx= context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //query buat table
+        /*
+        CREATE TABLE tbl_destinasi (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nama VARCHAR(50),
+            alamat TEXT,
+            jam VARCHAR(30)
+        );
+         */
+        //QUERY DI ATAS DI UBAH KEDALAM BENTURK STRING SEBAGAI BERIKUT
+
+        String query = "CREATE TABLE " + TABLE_NAME + " (" +
+                FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                FIELD_NAMA + " VARCHAR(50), " +
+                FIELD_ALAMAT + " TEXT, " +
+                FIELD_JAM + " VARCHAR(30)" +
+                ");"
+                ;
+        db.execSQL(query);
+        //harus ada spasi setelah kolom ( FIELD_ID + "(spasi) INTEGER PRIMARY KEY AUTOINCREMENT,)
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
 
+    public long tambahDATA(String nama, String alamat, String jam) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(FIELD_NAMA, nama);
+        cv.put(FIELD_ALAMAT, alamat);
+        cv.put(FIELD_JAM, jam);
+
+        long eksekusi = db.insert(TABLE_NAME, null, cv);
+        return eksekusi;
     }
 }
