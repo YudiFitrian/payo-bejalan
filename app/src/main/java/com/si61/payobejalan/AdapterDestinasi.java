1771cd133219ebfecfe38df8b50336e20363d33d
Yudi Fitriansyah <cleveryf098@gmail.com>
2023-03-29 17:31:02 +0700

    Payo Bejalan 23/03(3)

---
+++ app/src/main/java/com/si61/payobejalan/AdapterDestinasi.java
@@ -0,0 +1,4 @@
+package com.si61.payobejalan;
+
+public class AdapterDestinasi {
+}
--- app/src/main/java/com/si61/payobejalan/MainActivity.java
+++ app/src/main/java/com/si61/payobejalan/MainActivity.java
@@ -1,19 +1,25 @@
 package com.si61.payobejalan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fabTambah;
     private RecyclerView rvDestinasi;
     private MyDatabaseHelper myDB;
+    private ArrayList<String> arrNama, arrAlamat, arrJam;
+    private AdapterDestinasi adDestinasi;
 
 
     @Override
@@ -35,10 +41,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void SQLiteToArrayList(){
-
+        Cursor  varCursor = myDB.bacaDataDestinasi();
+        if(varCursor.getCount() == 0){
+            Toast.makeText(this, "Data Tidak Tersedia!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            while (varCursor.moveToNext()){
+                arrNama.add(varCursor.getString(1));
+                arrAlamat.add(varCursor.getString(2));
+                arrJam.add(varCursor.getString(3))
+            }
+        }
     }
 
     private void tampilDestinasi(){
+        arrNama = new ArrayList<>();
+        arrAlamat = new ArrayList<>();
+        arrJam = new ArrayList<>();
 
+        SQLiteToArrayList();
+
+        adDestinasi = new AdapterDestinasi(MainActivity.this, arrNama, arrAlamat, arrJam);
+        rvDestinasi.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        rvDestinasi.setAdapter(adDestinasi);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tampilDestinasi();
     }
 }
