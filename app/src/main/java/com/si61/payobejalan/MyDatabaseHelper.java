1d74e734557fa92203bdaf3621ccbd47521e146f
Yudi Fitriansyah <cleveryf098@gmail.com>
2023-03-29 17:12:31 +0700

    Payo Bejalan 23/03(2)

--- app/src/main/java/com/si61/payobejalan/MyDatabaseHelper.java
+++ app/src/main/java/com/si61/payobejalan/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.si61.payobejalan;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -66,5 +67,17 @@ public class MyDatabaseHelper extends SQLiteOpenHelper{
 
         long eksekusi = db.insert(TABLE_NAME, null, cv);
         return eksekusi;
+    }
+
+    public Cursor bacaDataDestinasi(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+
+        Cursor varCursor = null;
+        if(db!=null){
+            varCursor = db.rawQuery(query, null);
+        }
+
+        return varCursor;
     }
 }
